3124ba24cab5d41d5f8e709801bda78798407da6
DatGuard <67444984+DatGuard@users.noreply.github.com>
2020-10-27 15:39:47 +0200

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -1,6 +1,6 @@
 ideologies = [
     {
-        "name": "Dark Age Regressionist",
+        "name": "TBA",
         "stats": {
             "econ": 20,
             "dipl": 40,
@@ -16,38 +16,4 @@ ideologies = [
 			"mili": 70,
         }
     },
-	 {
-        "name": "Ethical Darwinism",
-        "stats": {
-            "econ": -100,
-            "dipl": -100,
-            "civi": 150,
-            "morl": 200,
-			"ordr": 200,
-			"tech": 50,
-			"hier": -100,
-			"rlgs": -100,
-			"rgnl": -100,
-			"govt": -100,
-			"cult": -100,
-			"mili": -50,
-        }
-    },
-	 {
-        "name": "Agressivism",
-        "stats": {
-            "econ": 0,
-            "dipl": 100,
-            "civi": 100,
-            "morl": -50,
-			"ordr": 150,
-			"tech": 25,
-			"hier": 90,
-			"rlgs": 90,
-			"rgnl": 50,
-			"govt": 50,
-			"cult": 100,
-			"mili": 100,
-        }
-    }
 ];
